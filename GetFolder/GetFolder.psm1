ec934f7fb1f863ba59c68ae04f17d234ec48fccc
tankcr <kristopher.roy@gmail.com>
2017-07-26 17:38:07 -0700

    Get File and Folder Module
    This is a module that add user interactive get file and folder functions to any script

--- GetFolder/GetFolder.psm1
+++ GetFolder/GetFolder.psm1
@@ -1,6 +1,41 @@
 <#
-	My Function
+	This Function creates a dialogue to return a Folder Path
 #>
-function Get-Function {
+function Get-Folder {
+    param([string]$Description="Select Folder to place results in",[string]$RootFolder="Desktop")
+
+ [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") |
+     Out-Null     
+
+   $objForm = New-Object System.Windows.Forms.FolderBrowserDialog
+        $objForm.Rootfolder = $RootFolder
+        $objForm.Description = $Description
+        $Show = $objForm.ShowDialog()
+        If ($Show -eq "OK")
+        {
+            Return $objForm.SelectedPath
+        }
+        Else
+        {
+            Write-Error "Operation cancelled by user."
+        }
 
 }
+
+<#
+	This Function creates a dialogue to return a File
+#>
+
+function Get-FileName($initialDirectory)
+{
+	[System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+	$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.FileName = "Select User List Input File"
+    #can be set to filter file types
+	#$OpenFileDialog.filter = "CSV (*.csv)| *.csv"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    Return $OpenFileDialog.FileName
+}
+
+Export-ModuleMember -Function 'Get-*'
