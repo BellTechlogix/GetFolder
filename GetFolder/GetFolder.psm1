0ae6d613f191080c35d22affa81ed395be604c32
tankcr <kristopher.roy@gmail.com>
2017-07-27 13:33:15 -0700

    Added Switches
    Added switches to allow grabbing objects, and selecting file types

--- GetFolder/GetFolder.psm1
+++ GetFolder/GetFolder.psm1
@@ -19,23 +19,78 @@ function Get-Folder {
         {
             Write-Error "Operation cancelled by user."
         }
-
 }
 
+
+
+# jrich method - this allows the developer good control with high odds they wont need to google for help
 <#
 	This Function creates a dialogue to return a File
+	.example
+	get-filename -filter "Name | *.filter"
+	.example
+	#something more complex
+	
 #>
 
-function Get-FileName($initialDirectory)
+#function Get-FileName($initialDirectory)
+#{
+#  param(
+    #File filter param. Review examples for the format and check the notes for the MSDN link on the syntax
+#    $Filter = "All Files | *.*",
+#    $Title="Select User List Input File"
+#    )
+	
+#	[System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+#	$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+#  $OpenFileDialog.initialDirectory = $initialDirectory
+#  $OpenFileDialog.FileName = $title
+#	$OpenFileDialog.filter = $filter 
+#
+#  $OpenFileDialog.ShowDialog() | Out-Null
+#  Return $OpenFileDialog.FileName
+#}
+
+
+
+#Alternative function -Szeraax
+#Adopted as Primary function - 27July17
+#Modified by TankCR
+<#
+	This Function creates a dialogue to return a File
+	.example
+	get-filename -filter "csv"
+Will create an OpenFileDialog box with the filter: "CSV (*.csv)" (which shows only .csv files)
+#>
+function Get-FileName
 {
+  param([Parameter(Mandatory=$false)][string]$Filter, [Parameter(Mandatory=$false)][switch]$Obj,[Parameter(Mandatory=$False)][string]$Title)
+  #  Mandatory = $false
+  #  $Filter = "All Files | *.*",
+  #  [switch]$Obj,
+  #  $Title="Select User List Input File"
+  #)
+    if(!($Title)) { $Title="Select Input File"}
+  
 	[System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
 	$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
-    $OpenFileDialog.initialDirectory = $initialDirectory
-    $OpenFileDialog.FileName = "Select User List Input File"
-    #can be set to filter file types
-	#$OpenFileDialog.filter = "CSV (*.csv)| *.csv"
-    $OpenFileDialog.ShowDialog() | Out-Null
-    Return $OpenFileDialog.FileName
+  $OpenFileDialog.initialDirectory = $initialDirectory
+  $OpenFileDialog.FileName = $Title
+  #can be set to filter file types
+  IF($Filter -ne $null){
+  $FilterString = '{0} (*.{1})|*.{1}' -f $Filter.ToUpper(), $Filter
+	$OpenFileDialog.filter = $FilterString}
+  if(!($Filter)) { $Filter = "All Files (*.*)| *.*"
+  $OpenFileDialog.filter = $Filter
+  }
+  $OpenFileDialog.ShowDialog() | Out-Null
+  IF($OBJ){
+  $fileobject = GI -Path $OpenFileDialog.FileName.tostring()
+  Return $fileObject
+  }
+  else{Return $OpenFileDialog.FileName}
 }
+
+
 
 Export-ModuleMember -Function 'Get-*'
